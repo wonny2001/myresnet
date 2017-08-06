692d4be7a8ea27d5acccdca999dbadb939d326a3
rainman <wonny2001@gmail.com>
2017-08-07 00:48:07 +0900

    new commit

---
+++ lab-11-7-mnist_cnn_layers_resnet.py
@@ -0,0 +1,205 @@
+# Lab 11 MNIST and Deep learning CNN
+import tensorflow as tf
+# import matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+tf.set_random_seed(777)  # reproducibility
+
+# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("MNIST_tw/", one_hot=True, validation_size=1000)
+
+# Check out https://www.tensorflow.org/get_started/mnist/beginners for
+# more information about the mnist dataset
+
+# hyper parameters
+learning_rate = 0.001
+training_epochs = 15000
+batch_size = 100
+
+#28x28 -> 24x24
+WH = 24
+SEC_WH = 12
+THIRD_WH = 6
+# FOURTH_WH = 4 : origin value
+FOURTH_WH = 3
+INPUT_SIZE = WH*WH
+LABELS = 10
+
+class Model:
+
+    def __init__(self, sess, name):
+        self.sess = sess
+        self.name = name
+        self._build_net()
+
+    def _build_net(self):
+        with tf.variable_scope(self.name):
+            # dropout (keep_prob) rate  0.7~0.5 on training, but should be 1
+            # for testing
+            self.training = tf.placeholder(tf.bool)
+
+            # input place holders
+            self.X = tf.placeholder(tf.float32, [None, INPUT_SIZE])
+
+            # img 28x28x1 (black/white), Input Layer
+            X_img = tf.reshape(self.X, [-1, WH, WH, 1])
+            self.Y = tf.placeholder(tf.float32, [None, 10])
+            conv0 = tf.layers.conv2d(inputs=X_img, filters=32, kernel_size=[3, 3],
+                                     padding="SAME", activation=tf.nn.relu)
+#layer1
+            with tf.variable_scope("conv_blocks_1"):
+
+                bn1_1 = tf.layers.batch_normalization(conv0)
+                conv1_1 = tf.layers.conv2d(inputs=bn1_1, filters=32, kernel_size=[3, 3],
+                                         padding="SAME", activation=tf.nn.relu)
+                bn1_2 = tf.layers.batch_normalization(conv1_1)
+                conv1_2 = tf.layers.conv2d(inputs=bn1_2, filters=32, kernel_size=[3, 3],
+                                         padding="SAME", activation=tf.nn.relu)
+
+            last2step = conv0
+            justbefore = conv1_2
+            for i in range(100):
+                # layer2
+                with tf.variable_scope('conv_blocks_%d' % (i + 2), reuse=False):
+                    # with tf.variable_scope('block1', reuse=False):
+                    last2step = last2step + justbefore
+                    bn1 = tf.layers.batch_normalization(last2step, name = "bn1")
+                    conv1 = tf.layers.conv2d(inputs=bn1, filters=32, kernel_size=[3, 3],
+                                               padding="SAME", activation=tf.nn.relu, name = "block1")
+                    # with tf.variable_scope('block1', reuse=False):
+                    bn2 = tf.layers.batch_normalization(conv1, name = "bn2")
+                    conv2 = tf.layers.conv2d(inputs=bn2, filters=32, kernel_size=[3, 3],
+                                               padding="SAME", activation=tf.nn.relu, name = "block2")
+                    justbefore  = conv2
+
+
+
+# #layer2
+#             with tf.variable_scope("conv2_1"):
+#                 combine = conv0 + conv1_2
+#                 bn2_1 = tf.layers.batch_normalization(combine)
+#                 conv2_1 = tf.layers.conv2d(inputs=bn2_1, filters=32, kernel_size=[3, 3],
+#                                          padding="SAME", activation=tf.nn.relu)
+#             with tf.variable_scope("conv2_2"):
+#                 bn2_2 = tf.layers.batch_normalization(conv2_1)
+#                 conv2_2 = tf.layers.conv2d(inputs=bn2_2, filters=32, kernel_size=[3, 3],
+#                                          padding="SAME", activation=tf.nn.relu)
+#
+# # layer3
+#             with tf.variable_scope("conv3_1"):
+#                 combine = combine + conv2_2
+#                 bn3_1 = tf.layers.batch_normalization(combine)
+#                 conv3_1 = tf.layers.conv2d(inputs=bn3_1, filters=32, kernel_size=[3, 3],
+#                                            padding="SAME", activation=tf.nn.relu)
+#             with tf.variable_scope("conv3_2"):
+#                 bn3_2 = tf.layers.batch_normalization(conv3_1)
+#                 conv3_2 = tf.layers.conv2d(inputs=bn3_2, filters=32, kernel_size=[3, 3],
+#                                            padding="SAME", activation=tf.nn.relu)
+
+            # # layer4
+            # with tf.variable_scope("conv4_1"):
+            #     combine = combine + conv3_2
+            #     bn4_1 = tf.layers.batch_normalization(combine)
+            #     conv4_1 = tf.layers.conv2d(inputs=bn4_1, filters=32, kernel_size=[3, 3],
+            #                                padding="SAME", activation=tf.nn.relu)
+            # with tf.variable_scope("conv4_2"):
+            #     bn4_2 = tf.layers.batch_normalization(conv4_1)
+            #     conv4_2 = tf.layers.conv2d(inputs=bn4_2, filters=32, kernel_size=[3, 3],
+            #                                padding="SAME", activation=tf.nn.relu)
+            #
+            #
+            # # layer5
+            # with tf.variable_scope("conv5_1"):
+            #     combine = combine + conv4_2
+            #     bn5_1 = tf.layers.batch_normalization(combine)
+            #     conv5_1 = tf.layers.conv2d(inputs=bn5_1, filters=32, kernel_size=[3, 3],
+            #                                padding="SAME", activation=tf.nn.relu)
+            # with tf.variable_scope("conv5_2"):
+            #     bn5_2 = tf.layers.batch_normalization(conv5_1)
+            #     conv5_2 = tf.layers.conv2d(inputs=bn5_2, filters=32, kernel_size=[3, 3],
+            #                                padding="SAME", activation=tf.nn.relu)
+
+            with tf.variable_scope("conv_combine"):
+                bn_com = tf.layers.batch_normalization(justbefore)
+
+                # Convolutional Layer #2 and Pooling Layer #2
+                conv_com = tf.layers.conv2d(inputs=bn_com, filters=128, kernel_size=[3, 3],strides=[4,4],
+                                         padding="same", activation=tf.nn.relu)
+                pool_com = tf.layers.max_pooling2d(inputs=conv_com, pool_size=[2, 2],
+                                                padding="same", strides=2)
+
+            with tf.variable_scope("Last_Lv"):
+
+                # Dense Layer with Relu
+                flat = tf.reshape(pool_com, [-1, 128 * FOURTH_WH * FOURTH_WH])
+                dense4 = tf.layers.dense(inputs=flat,
+                                         units=625, activation=tf.nn.relu)
+                dropout4 = tf.layers.dropout(inputs=dense4,
+                                             rate=0.5, training=self.training)
+
+                # Logits (no activation) Layer: L5 Final FC 625 inputs -> 10 outputs
+                self.logits = tf.layers.dense(inputs=dropout4, units=10)
+
+            print('1 cycle complete')
+
+        # define cost/loss & optimizer
+        self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+            logits=self.logits, labels=self.Y))
+        self.optimizer = tf.train.AdamOptimizer(
+            learning_rate=learning_rate).minimize(self.cost)
+
+        correct_prediction = tf.equal(
+            tf.argmax(self.logits, 1), tf.argmax(self.Y, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        print('1 _build_net complete')
+
+    def predict(self, x_test, training=False):
+        print('predict')
+        return self.sess.run(self.logits,
+                             feed_dict={self.X: x_test, self.training: training})
+
+    def get_accuracy(self, x_test, y_test, training=False):
+        print('get_accuracy')
+        return self.sess.run(self.accuracy,
+                             feed_dict={self.X: x_test,
+                                        self.Y: y_test, self.training: training})
+
+    def train(self, x_data, y_data, training=True):
+        # print('train')
+
+        return self.sess.run([self.cost, self.optimizer], feed_dict={
+            self.X: x_data, self.Y: y_data, self.training: training})
+
+# initialize
+sess = tf.Session()
+m1 = Model(sess, "m1")
+
+sess.run(tf.global_variables_initializer())
+merged_summary = tf.summary.merge_all()
+writer = tf.summary.FileWriter("./logs/resnet")
+writer.add_graph(sess.graph)  # Show the graph
+
+print('Learning Started!')
+
+# train my model
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+
+    # summary, _ = sess.run([merged_summary, train], feed_dict={X: x_data, Y: y_data})
+    # writer.add_summary(summary, global_step=step)
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        c, _ = m1.train(batch_xs, batch_ys)
+        avg_cost += c / total_batch
+
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+
+print('Learning Finished!')
+
+
+
+# Test model and check accuracy
+print('Accuracy:', m1.get_accuracy(mnist.test.images, mnist.test.labels))
